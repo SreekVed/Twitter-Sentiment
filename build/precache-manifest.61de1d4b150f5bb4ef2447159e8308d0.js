f42a3cf88037e724e55c02e9b0839495ceb56e95
Sreekar Vedula <sreekar.vedula@gmail.com>
2020-07-22 18:44:16 -0400

    Update API

---
+++ build/precache-manifest.61de1d4b150f5bb4ef2447159e8308d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d6aed91575904e738ef859c7400c7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "229c90d7592f0a303d96",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9a94dfe777fe3a09e2b7",
+    "url": "/static/css/main.dac4418d.chunk.css"
+  },
+  {
+    "revision": "229c90d7592f0a303d96",
+    "url": "/static/js/2.a54d05da.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a54d05da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a94dfe777fe3a09e2b7",
+    "url": "/static/js/main.f6f5c6de.chunk.js"
+  },
+  {
+    "revision": "e6aaedd62849837d70ef",
+    "url": "/static/js/runtime-main.a85180cb.js"
+  }
+]);
